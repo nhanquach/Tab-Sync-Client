d8f9af36693e2886e50ea2f1dfdb7d9c207b3f21
nhan.quach <nhan.quach@gooddata.com>
2023-08-02 22:18:58 +0700

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
+import {
+  ITab,
+  getArchivedTabs,
+  getOpenTabs,
+  onArchivedTabChange,
+  onOpenTabChange,
+  archiveOpenTabs,
+  removeArchivedTabs,
+} from "./clientApp";
+
+import "@picocss/pico";
+import UrlList from "./components/UrlList";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tabs, setTabs] = useState<Array<ITab>>([]);
+  const [filteredTabs, setFilteredTabs] = useState<Array<ITab>>([]);
+  const [archivedTabs, setArchivedTabs] = useState<Array<ITab>>([]);
+
+  const [searchString, setSearchString] = useState();
+
+  useEffect(() => {
+    onOpenTabChange(() => {
+      getOpenTabs().then(
+        (openTabs) => {
+          setTabs(openTabs);
+
+          if (filteredTabs.length === 0) {
+            setFilteredTabs(openTabs);
+          }
+        },
+        () => {}
+      );
+    });
+
+    onArchivedTabChange(() => {
+      getArchivedTabs().then(
+        (archived) => {
+          setArchivedTabs(archived);
+        },
+        () => {}
+      );
+    });
+
+    setLoading(false);
+  }, [filteredTabs.length]);
+
+  const handleSearch = (e: any) => {
+    setSearchString(e.target.value);
+
+    if (e.target.value) {
+      const newTabs = tabs.filter((tab) =>
+        tab.title.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      setFilteredTabs(newTabs);
+    } else {
+      setFilteredTabs(tabs);
+    }
+  };
+
+  const clearOpenTabs = (e: React.MouseEvent) => {
+    e.preventDefault();
+    archiveOpenTabs();
+  };
+
+  const clearArchivedTabs = (e: React.MouseEvent) => {
+    e.preventDefault();
+    removeArchivedTabs();
+  };
+
+  return (
+    <div>
+      <nav className="container-fluid" style={{ alignItems: "center" }}>
+        <h1 style={{ margin: "10px 0" }}>Tab Sync</h1>
+      </nav>
+
+      <div className="container">
+        <input
+          type="text"
+          placeholder="search..."
+          value={searchString}
+          onChange={handleSearch}
+        />
+      </div>
+
+      <UrlList
+        urls={filteredTabs}
+        onClear={clearOpenTabs}
+        header="Open Tabs"
+        isLoading={loading}
+      />
+
+      <UrlList
+        urls={archivedTabs}
+        onClear={clearArchivedTabs}
+        header="Archived Tabs"
+        isLoading={loading}
+      />
+    </div>
+  );
+}
+
+export default App;
